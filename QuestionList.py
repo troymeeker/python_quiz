af217f8a3716360790b944f071c5926733b3c60f
troymeeker <troymeeker1@gmail.com>
2022-08-26 11:41:54 -0700

    question added to quiz

--- QuestionList.py
+++ QuestionList.py
@@ -3,12 +3,15 @@ from Question import Question
 questions_list = [
     "What is the least populated State in the USA?\n (a) Alaska\n (b) Connecticut\n (c) Wyoming\n (d) Iowa \n\n ", 
     "What is the largest animal of all time?\n(a) Blue Whale \n(b) Chinese Giant Salamander \n(c) Giant squid \n(d) Argentinosaurus \n\n", 
+    "What was the first state in the United States\n(a) Virginia \n(b) California\n(c) Maryland \n(d) Delaware\n\n "
   
 ]
 
 questions = [
     Question(questions_list[0], "c"),
-    Question(questions_list[1], "a")
+    Question(questions_list[1], "a"),
+    Question(questions_list[2], "d")
+
 ]
 
 def run_test(questions):
