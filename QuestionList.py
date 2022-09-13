bfcdefcbc2e9a273704a572c968bf51e017ad537
troymeeker <troymeeker1@gmail.com>
2022-09-13 13:08:39 -0700

    started fresh w new questions, and new format

--- QuestionList.py
+++ QuestionList.py
@@ -1,34 +1,40 @@
-from calendar import c
-from Question import Question
-questions_list = [
-    "What is the least populated State in the USA?\n (a) Alaska\n (b) Connecticut\n (c) Wyoming\n (d) Iowa \n\n ", 
-    "What is the largest animal of all time?\n(a) Blue Whale \n(b) Chinese Giant Salamander \n(c) Giant squid \n(d) Argentinosaurus \n\n", 
-    "What was the first state in the United States\n(a) Virginia \n(b) California \n(c) Maryland \n(d) Delaware\n\n ", 
-    "What is the most populated state in the USA?\n(a) New York \n(b) California \n(c) Georgia \n(d) Washington\n\n"
-  
-]
+from string import ascii_lowercase
 
-questions = [
-    Question(questions_list[0], "c"),
-    Question(questions_list[1], "a"),
-    Question(questions_list[2], "d"), 
-    Question(questions_list[3], "b")
+questions = {
+    "What is the 2nd tallest Mountain in the world":
+        ["K2", "Kilimanjaro", "Denali", "Mt Hood"],
+    "What is the least populated state in the US":
+        ["Wyoming", "Alaska", "Montana", "Hawaii"],
+    "Who is the oldest active player in the NFL":
+        ["Tom Brady", "John Elway", "Joe Burrow", "Adam Vinatieri", "Aaron Rodgers"],
+    "Who was the youngest President of the United States":
+        ["Theodore Roosevelt", "Barack Obama", "Abraham Lincoln", "George W Bush"],
 
-]
+}
 
-def run_test(questions):
-    score = 0
-    for q in questions:
-        answer = input(q.question)
-        if answer == q.answer:
-            score +=1 
+score = 0
+for num, (question, answers) in enumerate(questions.items(), start=1):
+    print(f"\nQuestion {num}:")
+    print(f"{question}?")
+    correct = answers[0]
+    
+    labeled_answers = dict(zip(ascii_lowercase, sorted(answers)))
+    # sorted_answers = sorted(answers)
+    for label, answer in labeled_answers.items():
+        print(f" {label}) {answer}")
 
-    if int(score) == 0:
-        print("You got " + str(score) + "/" + str(len(questions)) + " questions correct! Try again")
-    elif int(score) < 3:
-        print("You got " + str(score) + "/" + str(len(questions)) + " questions correct! You did Okay, Study up!")
+    answer_label = input("\nChoice? ")
+    answer = labeled_answers.get(answer_label)
+    if answer == correct:
+        score += 1
+        print(" Correct! ")
+        
     else:
-        print("You got " + str(score) + "/" + str(len(questions)) + " questions correct! Great Job!")            
+        print(f"The correct answer is {correct!r}, not {answer!r} ")
 
-
-run_test(questions)
+if int(score) == 0:
+    print("You got " + str(score) + "/" + str(len(questions)) + " questions correct! Try again")
+elif int(score) < 3:
+    print("You got " + str(score) + "/" + str(len(questions)) + " questions correct! You did Okay, Study up!")
+else:
+    print("You got " + str(score) + "/" + str(len(questions)) + " questions correct! Great Job!")            
